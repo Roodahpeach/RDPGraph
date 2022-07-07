ca86f03fe192e312ee4f9057796afebc95af9058
KKM <kkm@comizoa.com>
2022-07-07 11:37:28 +0900

    feat: 기본 셋업

---
+++ RDPGraph/RDPGraph/MainForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MaterialSkin;
+using MaterialSkin.Controls;
+
+using ScottPlot;
+using System.Collections;
+
+namespace RDPGraph
+{
+    public partial class MainForm : MaterialForm
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey500, Primary.BlueGrey900, Primary.BlueGrey500, Accent.Red200, TextShade.WHITE);
+        }
+    }
+}
